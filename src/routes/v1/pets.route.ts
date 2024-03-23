c9e42366035eb08b38c3a7f5bf7036dd586201bd
maruwtc <chriswong0621@gmail.com>
2024-03-23 19:13:35 +0800

    feat: remove duplicated validation function

--- src/routes/v1/pets.route.ts
+++ src/routes/v1/pets.route.ts
@@ -1,15 +1,14 @@
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import { getAllPets, getPetById, createPet, updatePet, deletePet } from '../../controllers/pets.controller'
-import { validatePet } from '../../validations/validation'
 import { basicAuth } from '../../middlewares/authorization'
 
 const router = new Router({ prefix: '/api/v1' })
 
 router.get('/pets', getAllPets)
 router.get('/pets/:id', getPetById)
-router.post('/pets', basicAuth, bodyParser(), validatePet, createPet)
-router.put('/pets/:id', basicAuth, bodyParser(), validatePet, updatePet)
+router.post('/pets', basicAuth, bodyParser(), createPet)
+router.put('/pets/:id', basicAuth, bodyParser(), updatePet)
 router.delete('/pets/:id', basicAuth, deletePet)
 
 export { router }
--- src/routes/v1/users.route.ts
+++ src/routes/v1/users.route.ts
@@ -1,15 +1,14 @@
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from '../../controllers/users.controller'
-import { validateUser } from '../../validations/validation'
 import { basicAuth } from '../../middlewares/authorization'
 
 const router = new Router({ prefix: '/api/v1' })
 
 router.get('/users', getAllUsers)
 router.get('/users/:id', getUserById)
-router.post('/users', basicAuth, bodyParser(), validateUser, createUser)
-router.put('/users/:id', basicAuth, bodyParser(), validateUser, updateUser)
+router.post('/users', basicAuth, bodyParser(), createUser)
+router.put('/users/:id', basicAuth, bodyParser(), updateUser)
 router.delete('/users/:id', basicAuth, deleteUser)
 
 export { router }
