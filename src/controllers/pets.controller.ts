ab839e3fb08a8711feac9497c9d616ffc448db5a
maruwtc <chriswong0621@gmail.com>
2024-03-23 15:47:31 +0800

    refactor: restructure project structure

---
+++ src/controllers/pets.controller.ts
@@ -0,0 +1,89 @@
+import { Pet, PetInput } from "../models/pets.model"
+
+const getAllPets = async (ctx: any) => {
+    try {
+        const pets = await Pet.find()
+        ctx.body = pets
+    } catch (error: any) {
+        ctx.status = 500
+        ctx.body = { message: error.message }
+    }
+}
+
+const getPetById = async (ctx: any) => {
+    try {
+        const pet = await Pet.findOne({ id: ctx.params.id })
+        if (pet) {
+            ctx.body = pet
+        } else {
+            ctx.status = 404
+            ctx.body = { message: 'Pet not found' }
+        }
+    } catch (error: any) {
+        ctx.status = 500
+        ctx.body = { message: error.message }
+    }
+}
+
+const createPet = async (ctx: any) => {
+    try {
+        const { name, category, age, breed, location, image, description }: PetInput = ctx.request.body
+        const pet = new Pet({
+            id: Math.floor(Math.random() * 100),
+            name,
+            category,
+            age,
+            breed,
+            location,
+            image,
+            description
+        })
+        await pet.save()
+        ctx.body = pet
+    } catch (error: any) {
+        ctx.status = 500
+        ctx.body = { message: error.message }
+    }
+}
+
+const updatePet = async (ctx: any) => {
+    try {
+        const { name, category, age, breed, location, image, description }: PetInput = ctx.request.body
+        const pet = await Pet.findOne({ id: ctx.params.id })
+        if (pet) {
+            pet.name = name
+            pet.category = category
+            pet.age = age
+            pet.breed = breed
+            pet.location = location
+            pet.image = image
+            pet.description = description
+            await pet.save()
+            ctx.body = pet
+        } else {
+            ctx.status = 404
+            ctx.body = { message: 'Pet not found' }
+        }
+    } catch (error: any) {
+        ctx.status = 500
+        ctx.body = { message: error.message }
+    }
+}
+
+const deletePet = async (ctx: any) => {
+    try {
+        const pet = await Pet.findOne({ id: ctx.params.id })
+        if (pet) {
+            await pet.deleteOne({ id: ctx.params.id })
+            ctx.body = { message: 'Pet deleted' }
+        } else {
+            ctx.status = 404
+            ctx.body = { message: 'Pet not found' }
+        }
+    } catch (error: any) {
+        ctx.status = 500
+        ctx.body = { message: error.message }
+    }
+}
+
+export { getAllPets, getPetById, createPet, updatePet, deletePet }
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import Koa from 'koa'
+import Router, { RouterContext } from 'koa-router'
+import json from 'koa-json'
+import { connect as ConnectDB } from './config/database'
+import { router as users } from './routes/users.route'
+import { router as pets } from './routes/pets.route'
+
+const app: Koa = new Koa()
+
+ConnectDB().catch(console.dir)
+
+const router: Router = new Router()
+
+router.get('/', (ctx: RouterContext) => {
+    ctx.body = 'Welcome to the API!'
+})
+
+app.use(json())
+app.use(router.routes())
+app.use(users.routes())
+app.use(pets.routes())
+
+app.use(async (ctx: RouterContext, next: any) => {
+    try {
+        await next()
+        if (ctx.status === 404) {
+            ctx.status = 404
+            ctx.body = 'Page not found'
+        }
+    } catch (err: any) {
+        ctx.body = { message: err.message }
+    }
+})
+
+app.listen(8000, () => {
+    console.log('Server is running at http://localhost:8000')
+})
