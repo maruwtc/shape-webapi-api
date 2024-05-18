c3699a822b4763518891b1da7b2fa6814fd7732d
maruwtc <chriswong0621@gmail.com>
2024-05-18 15:45:24 +0800

    feat: change to use firebase for user auth

--- src/middlewares/hashing.ts
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const hashedPassword = ( password: any) => {
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
-    return hash
-}
