73ea408bbc493601a3278567f584d77f03281773
maruwtc <chriswong0621@gmail.com>
2024-05-19 18:41:14 +0800

    chore: Update authentication routes and middleware for Firebase integration

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,13 +1,14 @@
 import Router from 'koa-router'
-import { handleLogin, handleSignout, handleRegister, isAuthorized, getUser } from '../../middlewares/firebase'
+import { handleLogin, handleRegister, isAuth, getUser, deleteUser, uploadImage } from '../../middlewares/firebase'
 import bodyParser from 'koa-bodyparser'
 
 const router = new Router({ prefix: '/api/v1' })
 
 router.post('/signin', bodyParser(), handleLogin)
-router.post('/signout', bodyParser(), handleSignout)
 router.post('/register', bodyParser(), handleRegister)
-router.post('/checkauth', bodyParser(), isAuthorized)
+router.post('/checkauth', bodyParser(), isAuth)
 router.post('/getuser', bodyParser(), getUser)
+router.post('/deleteaccount', bodyParser(), deleteUser)
+router.post('/uploadimage', uploadImage)
 
 export { router }
