465cee92aa81ebb544bf15c3f12243f5a1b0676c
maruwtc <chriswong0621@gmail.com>
2024-05-24 15:21:06 +0800

    chore: Update config.sample.ts

---
+++ config.sample.ts
@@ -0,0 +1,13 @@
+export const config = {
+    // mongodb
+    mongodb_user: "",
+    mongodb_password: "",
+    mongodb_db: "",
+    // firebase
+    firebase_apiKey: "",
+    firebase_authDomain: "",
+    firebase_projectId: "",
+    firebase_storageBucket: "",
+    firebase_messagingSenderId: "",
+    firebase_appId: "",
+}
