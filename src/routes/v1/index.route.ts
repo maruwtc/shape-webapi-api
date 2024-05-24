d1e73e03315314322b176919a01f3eb4c0374b87
maruwtc <chriswong0621@gmail.com>
2024-05-24 15:22:56 +0800

    refactor: Remove deprecated authentication routes and middleware

--- src/routes/v1/index.route.ts
+++ src/routes/v1/index.route.ts
@@ -1,11 +1,9 @@
 import Router from 'koa-router';
-import { router as authRouter } from './auth.route';
 import { router as petsRouter } from './pets.route';
 import { router as usersRouter } from './users.route';
 
 const router = new Router({ prefix: '/api/v1' });
 
-router.use(authRouter.routes());
 router.use(petsRouter.routes());
 router.use(usersRouter.routes());
 
